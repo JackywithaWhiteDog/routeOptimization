50f4c567fbcbf75dc8b30cdfcd825b202a6b0bc5
JackywithaWhiteDog <45003637+JackywithaWhiteDog@users.noreply.github.com>
2018-11-23 18:09:56 +0800

    Update PD107-1_Midterm-Project.cpp
    add class

--- PD107-1_Midterm-Project.cpp
+++ PD107-1_Midterm-Project.cpp
@@ -4,166 +4,212 @@
 #include <ctime>
 using namespace std;
 
+class point
+{
+	public:
+		float x, y;
+		int radius, danger;
+		init(int i, int j);
+		init(int i, int j, int r, int p);
+};
+
 const int PAIR = 2;
 
-int random(int upper, int lower);
+// return a random number from lower to upper
+int random(int lower, int upper);
 
-void randInfo(int &n, int &m, int &w, int &d, int* &x, int* &y, int* &r, int* &p, int start[], int end[]);
-void getInfo(int &n, int &m, int &w, int &d, int* &x, int* &y, int* &r, int* &p, int start[], int end[]);
-void showInfo(int n, int m, int w, int d, const int* x, const int* y, const int* r, const int* p, const int start[], const int end[]);
+// input information
+void getInfo(int& n, int& m, int& w, int& d, point* danPt, point& start, point& end);
+// randomly create information
+void randInfo(int& n, int& m, int& w, int& d, point* danPt, point& start, point& end);
 
-float ptDistance(float x1, float y1, float x2, float y2);
+// return the distance of two point
+float distance(point a, point b);
 
-float pointDanger(const float pt[], const int m, const int* x, const int* y, const int* r, const int* p);
-float lineDanger(float start[], float end[], const int m, const int* x, const int* y, const int* r, const int* p, float firstD = 1);
-float totalDanger(float start[], float end[], float corner[][PAIR], int corCnt, const int m, const int* x, const int* y, const int* r, const int* p);
+// check if the path is legal(all the point is in the map and does not exceed the distance)
+bool islegal(point start, point end, point* corner, int corCnt, int n, int d);
+
+// return the danger of a point
+float pointDanger(point pt, point* danPt, int m);
+// return the danger of a line
+float lineDanger(point from, point to, point* danPt, int m, float firstD = 1);
+// return the danger of a path
+float totalDanger(point start, point end, point* corner, int corCnt, point* danPt, int m);
 
 int main()
 {
 	srand(time(NULL));
+	clock_t startTime = clock();
 	
-	int mapLen = 0, ptCnt = 0, weight = 0, distance = 0;
-	int* x = nullptr;
-	int* y = nullptr;
-	int* r = nullptr;
-	int* p = nullptr;
-	int start[PAIR] = {0}, end[PAIR] = {0};
-	
-	/*
-	getInfo(mapLen, ptCnt, weight, distance, x, y, r, p, start, end);
-	/*/
-	randInfo(mapLen, ptCnt, weight, distance, x, y, r, p, start, end);
-//	showInfo(mapLen, ptCnt, weight, distance, x, y, r, p, start, end);
-	/**/
-	
-	delete [] x;
-	delete [] y;
-	delete [] r;
-	delete [] p;
+	int mapSize = 0, ptCnt = 0, weight = 0, distance = 0;
+	point* danPt = nullptr;
+	point start, end;
 	
 	return 0;
- } 
- 
+}
+
+point::init(int i, int j)
+{
+	x = i;
+	y = j;
+	radius = 0;
+	danger = 0;
+}
+point::init(int i, int j, int r, int p)
+{
+	x = i;
+	y = j;
+	radius = r;
+	danger = p;
+}
+
+// return a random number from lower to upper
 int random(int lower, int upper)
 {
-	return static_cast<int>((upper-lower)*static_cast<float>(rand())/RAND_MAX)+lower;
+	return static_cast<int>((upper-lower)*static_cast<float>(rand())/RAND_MAX+lower);
 }
 
-void getInfo(int &n, int &m, int &w, int &d, int* &x, int* &y, int* &r, int* &p, int start[], int end[])
+// input information
+void getInfo(int& n, int& m, int& w, int& d, point* danPt, point& start, point& end)
 {
 	cin >> n >> m >> w >> d;
-	x = new int [m];
-	y = new int [m];
-	r = new int [m];
-	p = new int [m];
+	
+	danPt = new point [m];
 	for(int i = 0; i < m; i++)
 	{
-		cin >> x[i] >> y[i] >> r[i] >> p[i];
+		cin >> danPt[i].x;
 	}
-	cin >> start[0] >> start[1] >> end[0] >> end[1];
+	for(int i = 0; i < m; i++)
+	{
+		cin >> danPt[i].y;
+	}
+	for(int i = 0; i < m; i++)
+	{
+		cin >> danPt[i].radius;
+	}
+	for(int i = 0; i < m; i++)
+	{
+		cin >> danPt[i].danger;
+	}
+	
+	
+	cin >> start.x >> start.y >> end.x >> end.y;
+	
+	start.init(start.x, start.y);
+	end.init(end.x, end.y);
 }
 
-void randInfo(int &n, int &m, int &w, int &d, int* &x, int* &y, int* &r, int* &p, int start[], int end[])
+// randomly create information
+void randInfo(int& n, int& m, int& w, int& d, point* danPt, point& start, point& end)
 {
 	n = random(1, 1000);
-	m = random(1, (n+1)*(n+1));
+	m = random(1, min((n+1)*(n+1), 10000));
 	w = random(0, 1000);
-	d = random(n, n*n); 
-	x = new int [m];
-	y = new int [m];
-	r = new int [m];
-	p = new int [m];
+
+	danPt = new point [m];
+	
 	for(int i = 0; i < m; i++)
 	{
-		x[i] = random(0, n);
-		y[i] = random(0, n);
-		r[i] = random(0, n);
-		p[i] = random(1, 1000);
+		danPt[i].init(random(0,n), random(0,n), random(0,n), random(1,1000));
 	}
-	start[0] = random(0, n);
-	start[1] = random(0, n);
-	end[0] = random(0, n);
-	end[1] = random(0, n);
+	
+	start.init(random(0,n), random(0,n));
+	end.init(random(0,n), random(0,n));
+	
+	d = random(max(n, static_cast<int>(distance(start, end))+1), min(10*n, static_cast<int>(distance(start, end))+1)); 
 }
 
-void showInfo(int n, int m, int w, int d, const int* x, const int* y, const int* r, const int* p, const int start[], const int end[])
+// return the distance of two point
+float distance(point a, point b)
 {
-	cout << n << " " << m << " " << w << " " << d << "\n";
-	for(int i = 0; i < m; i++)
+	return sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y));
+}
+
+// check if the path is legal(all the point is in the map and does not exceed the distance)
+bool islegal(point start, point end, point* corner, int corCnt, int n, int d)
+{
+	for(int i = 0; i < corCnt; i++)
 	{
-		cout << x[i] << " " << y[i] << " " << r[i] << " " << p[i] << "\n";
+		if(corner[i].x < 0 || corner[i].x > n || corner[i].y < 0 || corner[i].y > n)
+		{
+			return false;
+		}
 	}
-	cout << start[0] << " " << start[1] << " " << end[0] << " " << end[1] << "\n";
+	
+	float totalD = 0;
+	totalD += distance(start, corner[0]);
+	for(int i = 1; i < corCnt-1; i++)
+	{
+		totalD += distance(corner[i], corner[i+1]);
+	}
+	totalD += distance(corner[corCnt-1], end);
+	
+	if(totalD > d)
+	{
+		return false;
+	}
+	
+	return true;
 }
 
-float ptDistance(float x1, float y1, float x2, float y2)
-{
-	return sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
-}
-
-float pointDanger(const float pt[], const int m, const int* x, const int* y, const int* r, const int* p)
+// return the danger of a point
+float pointDanger(point pt, point* danPt, int m)
 {
 	float ptDanger = 0;
-	for(int k = 0; k < m; k++)
+	for(int i = 0; i < m; i++)
 	{
 		// if the position is in the influence radius of dangerous point, add the danger
-		if((pt[0]-x[k])*(pt[0]-x[k]) + (pt[1]-y[k])*(pt[1]-y[k]) < r[k]*r[k])
+		if(distance(pt, danPt[i]) < danPt[i].radius)
 		{
 			// danger (with one dangerous point) = (degree of danger) * [(radius) - (distance)] / (radius)
-			ptDanger += p[k] * (r[k] - ptDistance(pt[0], pt[1], x[k], y[k])) / r[k];
+			ptDanger += danPt[i].danger * (danPt[i].radius - distance(pt, danPt[i])) / danPt[i].radius;
 		}
 	}
 	return ptDanger;
 }
 
-float lineDanger(float start[], float end[], const int m, const int* x, const int* y, const int* r, const int* p, float firstD)
+// return the danger of a line
+float lineDanger(point from, point to, point* danPt, int m, float firstD)
 {
-	float d = ptDistance(start[0], start[1], end[0], end[1]);
+	float d = distance(from, to);
 	int checkCnt = static_cast<int>(d - firstD) + 1;
-	float** checkPt = new float* [checkCnt];
-	for(int i = 0; i < checkCnt; i++)
-	{
-		checkPt[i] = new float [PAIR];
-	}
+
+	point* checkPt = new point [checkCnt];
 	
-	checkPt[0][0] = start[0] + (firstD/d)*(end[0] - start[0]);
-	checkPt[0][1] = start[1] + (firstD/d)*(end[1] - start[1]);
-	
+	checkPt[0].init(from.x+(firstD/d)*(to.x - from.x), from.y+(firstD/d)*(to.y - from.y));
 	for(int i = 1; i < checkCnt; i++)
 	{
-		checkPt[i][0] = checkPt[i-1][0] + (end[0] - start[0])/d;
-		checkPt[i][1] = checkPt[i-1][1] + (end[1] - start[1])/d;
+		checkPt[i].init(checkPt[i-1].x+(to.x - from.x)/d, checkPt[i-1].y+(to.y - from.y)/d);
 	}
 	
 	float lnDanger = 0;
 	for(int i = 0; i < checkCnt; i++)
 	{
-		lnDanger += pointDanger(checkPt[i], m, x, y, r, p);
+		lnDanger += pointDanger(checkPt[i], danPt, m);
 	}
 	
-	for(int i = 0; i < checkCnt; i++)
-	{
-		delete [] checkPt[i];
-	}
 	delete [] checkPt;
 	
 	return lnDanger;
 }
 
-float totalDanger(float start[], float end[], float corner[][PAIR], int corCnt, const int m, const int* x, const int* y, const int* r, const int* p)
+// return the danger of a path
+float totalDanger(point start, point end, point* corner, int corCnt, point* danPt, int m)
 {
-	float ttDanger = 0;
+	float ttDanger = 0;	
+	ttDanger += lineDanger(start, corner[0], danPt, m);
 	
-	ttDanger += lineDanger(start, corner[0], m, x, y, r, p, 1);
-	float d = ptDistance(start[0], start[1], end[0], end[1]);
+	float d = distance(start, corner[0]);
 	float firstD = 1 - (d - static_cast<int>(d));
 	for(int i = 0; i < corCnt-1; i++)
 	{
-		ttDanger += lineDanger(corner[i], corner[i+1], m, x, y, r, p, firstD);
-		d = ptDistance(corner[i][0], corner[i][1], corner[i+1][0], corner[i+1][1]);
+		ttDanger += lineDanger(corner[i], corner[i+1], danPt, m, firstD);
+		d = distance(corner[i], corner[i+1]);
 		firstD = 1 - (d - static_cast<int>(d));
 	}
-	ttDanger += lineDanger(corner[corCnt-1], end, m, x, y, r, p, firstD);
+	ttDanger += lineDanger(corner[corCnt-1], end, danPt, m, firstD);
 	
 	return ttDanger;
-} 
+	
+}
+
